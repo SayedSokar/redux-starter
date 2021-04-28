12cc6fc0f0e0fe7ee7b0774a9c407c473e9eacf9
Sayed Sokar <sayed.ramadan@nour-ec.com>
2021-04-28 07:52:53 +0200

    final middleWare

--- src/store/configStore.js
+++ src/store/configStore.js
@@ -1,6 +1,8 @@
 import {configureStore} from '@reduxjs/toolkit'
 import reducer from './reducer'
 import logger from './middleware/logger'
+import api from './middleware/api'
+
 
 //---------------------- Create the store---------------------------------//
 // note that the configureStore is automatically setup the store to talke to redux devtools, so no need to mention devtools as a second argment of the store below.
@@ -9,6 +11,10 @@ import logger from './middleware/logger'
 export default function () {
     return configureStore({
         reducer, // it can be writen like (reducer: reducer), both are the same name, so we can just put reducer
-        middleware: [logger({destination: "console"})] // this properity is calling the Middleware logger.
+        middleware: [
+            logger({ destination: "console" }),
+            api
+        ] // this properity is calling the Middleware logger.
+        
     })
  }
--- src/store/middleware/logger.js
+++ src/store/middleware/logger.js
@@ -2,6 +2,6 @@
 const logger = param => store => next => action => { // if needed to send param to the middleware, this is how (note: param)
     console.log("Logging", param)
     
-    next(action)
+    next(action) // this line to execute the action, if not mentioned there will not be action
 }
 export default logger
